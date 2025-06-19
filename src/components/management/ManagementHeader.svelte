7ace2a29696368d71c876de83a5b9c9d8401861c
splimter <11891837+splimter@users.noreply.github.com>
2025-06-19 12:07:02 +0100

    click on profile bubble issue

--- src/components/management/ManagementHeader.svelte
+++ src/components/management/ManagementHeader.svelte
@@ -69,19 +69,21 @@
 
     <div class="flex items-center gap-4">
         <div class="flex rounded-full bg-gray-100 p-1">
-            <button class="w-8 h-8 flex items-center justify-center rounded-full text-sm font-medium transition-all
-                    {currentLang === 'EN' ? 'bg-white border-2 border-primary shadow-sm' : 'text-gray-600 hover:color-primary'}"
-                    onclick={() => setLanguage('EN')}>
-                EN
-            </button>
-            <button class="w-8 h-8 flex items-center justify-center rounded-full text-sm font-medium transition-all
-                    {currentLang === 'AR' ? 'bg-white border-2 border-primary shadow-sm' : 'text-gray-600 hover:color-primary'}"
-                    onclick={() => setLanguage('AR')}>
-                AR
-            </button>
+<!--TODO: lang-->
+
+<!--            <button class="w-8 h-8 flex items-center justify-center rounded-full text-sm font-medium transition-all-->
+<!--                    {currentLang === 'EN' ? 'bg-white border-2 border-primary shadow-sm' : 'text-gray-600 hover:color-primary'}"-->
+<!--                    onclick={() => setLanguage('EN')}>-->
+<!--                EN-->
+<!--            </button>-->
+<!--            <button class="w-8 h-8 flex items-center justify-center rounded-full text-sm font-medium transition-all-->
+<!--                    {currentLang === 'AR' ? 'bg-white border-2 border-primary shadow-sm' : 'text-gray-600 hover:color-primary'}"-->
+<!--                    onclick={() => setLanguage('AR')}>-->
+<!--                AR-->
+<!--            </button>-->
         </div>
 
-        <Button pill color="light" class="flex items-center gap-2 py-1 px-3">
+        <Button pill color="light" class="flex items-center gap-2 py-1 px-3" id="avatar_with_name">
             {#await getAvatar($user.shortname)}
                 <Avatar src={null} size="xs" class="ring-2 ring-white"/>
             {:then avatar}
@@ -92,7 +94,7 @@
 
             <span class="text-sm">{$user.shortname}</span>
 
-            <Dropdown simple>
+            <Dropdown simple triggeredBy="#avatar_with_name">
                 <DropdownItem onclick={(e) => goToProfile(e)}>
                     <div class="flex items-center gap-2">
                         <UserSolid size="sm" /> My Profile
